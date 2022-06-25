cd3c6094d0a7b7739395d646c5255f5f7ec03849
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-06-24 22:46:40 -0400

    login and logout functionality is working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -22,12 +22,34 @@ router.get("/", (req, res) => {
   })
     .then((dbPostData) => {
       // pass a single post object into the homepage template
-      res.render("homepage", dbPostData[0]);
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
     })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
